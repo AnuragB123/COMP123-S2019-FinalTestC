6e9ca5e8577105b02ed23dcd03d3501465c91183
Anurag Bhattacharya <abhatt66@my.centennialcollege.ca>
2019-08-12 14:53:46 -0400

    Begginnings of the final

---
+++ COMP123-S2019-FinalTestC/Objects/Identiy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * STUDENT NAME:
+ * STUDENT ID:
+ * DESCRIPTION: This is the Identity class which used with Charac
+*/
+namespace COMP123_S2019_FinalTestC.Objects
+{
+    public class Identiy
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ COMP123-S2019-FinalTestC/Objects/Skill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * STUDENT NAME:
+ * STUDENT ID:
+ * DESCRIPTION: This is the Skill Class to be used with Character Portfolio class
+*/
+namespace COMP123_S2019_FinalTestC.Objects
+{
+    public class Skill
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
